fd18c03b05f8e2e1724ab9004b07c83cb5f521e5
Colin Xiong <xiong.colin899@gmail.com>
2022-10-02 13:34:32 -0400

    Final python

--- python_backend_AI.py
+++ python_backend_AI.py
@@ -1,19 +1,32 @@
 import spacy
 nlp = spacy.load('en_core_web_sm')
-text = str(input())
+text = input("Enter the statement to be niced: ")
 
 about_text = (text)
 
 about_doc = nlp(about_text)
-    
-spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
         
-words = [token for token in about_doc if not token.is_stop and not token.is_punct]
-print (words)
+words = [token for token in about_doc]
 
-for token in words:
-    print (token, token.pos_)
+"""
+#words = [token for token in about_doc if not token.is_stop and not token.is_punct]
+#code used if implemented with website
+"""
     
+final_sentence = ""
+for token in words:
+    if token.pos_ == 'NOUN':
+        final_sentence += 'gentleman '
+    elif token.pos_ == 'ADJ':
+        final_sentence += 'amazing '
+    elif token.pos_ == 'ADV':
+        final_sentence += 'cheerfully '
+    elif token.pos_ == 'VERB':
+        final_sentence += 'helped '
+    else:
+        final_sentence += (str(token) + ' ')
+        
+"""
 n = []
 adj = []
 adv = []
@@ -27,3 +40,9 @@ for token in words:
         adv.append(token)
     elif token.pos_ == 'VERB':
         v.append(token)
+
+This code is used if implemented in the website. We use lists to provide a wider range
+of nice word choices.
+"""
+
+print("Here is a nicer way to say your statement: " + final_sentence)
