c408157413008dfe0ecfb6d96acd5ada24e86429
Colin Xiong <xiong.colin899@gmail.com>
2023-01-22 23:45:59 -0500

    Final Changes to python back-end and requests

--- my-app/src/components/MainSite/MainSite.js
+++ my-app/src/components/MainSite/MainSite.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './MainSite.css';
 import svg from './Vector.svg'
+import axios from 'axios';
 
 
 //Page HTML as react component
@@ -46,35 +47,18 @@ class MainSite extends React.Component{
 //Function to pull text of div in 
 function TextInput() {
     const inputed_text=document.getElementById("user-text").textContent;
-    console.log(inputed_text);
-    let words = inputed_text,
-    wordArray = words.toLowerCase().split(' ');
+    // console.log(inputed_text);
+    // let words = inputed_text,
+    // wordArray = words.toLowerCase().split(' ');
 
-    console.log(wordArray);
-
-    const bad_words = ["fuck", "dumbass", "acrotomophilia", 'anal', 'anilingus', 'anus', 'apeshit', 'arsehole', 'ass', 'asshole', 'assmunch', 'autoerotic', 'babeland', 'bangbros', 'bangbus', 'bareback', 'barenaked', 'bastard', 'bastardo', 'bastinado', 'bbw', 'bdsm', 'beaner', 'beaners', 'beastiality', 'bestiality', 'bimbos', 'birdlock', 'bitch', 'bitches', 'blowjob', 'blumpkin', 'bollocks', 'bondage', 'boner', 'boob', 'boobs', 'bukkake', 'bulldyke', 'bullshit', 'bunghole', 'busty', 'butt', 'buttcheeks', 'butthole', 'camgirl', 'camslut', 'camwhore', 'carpetmuncher', 'cialis', 'circlejerk', 'clit', 'clitoris', 'clusterfuck', 'cock', 'cocks', 'coprolagnia', 'coprophilia', 'cornhole', 'coon', 'coons', 'creampie', 'cum', 'cumming', 'cumshot', 'cumshots', 'cunnilingus', 'cunt', 'darkie', 'daterape', 'deepthroat', 'dendrophilia', 'dick', 'dildo', 'dingleberry', 'dingleberries', 'doggiestyle', 'doggystyle', 'dolcett', 'domination', 'dominatrix', 'dommes', 'dvda', 'ecchi', 'ejaculation', 'erotic', 'erotism', 'escort', 'eunuch', 'fag', 'faggot', 'fecal', 'felch', 'fellatio', 'feltch', 'femdom', 'figging', 'fingerbang', 'fingering', 'fisting', 'footjob', 'frotting', 'fuck', 'fuckin', 'fucking', 'fucktards', 'fudgepacker', 'futanari', 'gangbang', 'genitals', 'goatcx', 'goatse', 'gokkun', 'goodpoop', 'goregasm', 'grope', 'g-spot', 'guro', 'handjob', 'hardcore', 'hentai', 'homoerotic', 'honkey', 'hooker', 'horny', 'humping', 'incest', 'intercourse', 'jailbait', 'jigaboo', 'jiggaboo', 'jiggerboo', 'jizz', 'juggs', 'kike', 'kinbaku', 'kinkster', 'kinky', 'knobbing', 'livesex', 'lolita', 'lovemaking', 'masturbate', 'masturbating', 'masturbation', 'milf', 'mong', 'motherfucker', 'muffdiving', 'nambla', 'nawashi', 'negro', 'neonazi', 'nigga', 'nigger', 'nimphomania', 'nipple', 'nipples', 'nsfw', 'nude', 'nudity', 'nutten', 'nympho', 'nymphomania', 'octopussy', 'omorashi', 'orgasm', 'orgy', 'paedophile', 'paki', 'panties', 'panty', 'pedobear', 'pedophile', 'pegging', 'penis', 'pikey', 'pissing', 'pisspig', 'playboy', 'ponyplay', 'poof', 'poon', 'poontang', 'punany', 'poopchute', 'porn', 'porno', 'pornography', 'pthc', 'pubes', 'pussy', 'queaf', 'queef', 'quim', 'raghead', 'rape', 'raping', 'rapist', 'rectum', 'rimjob', 'rimming', 'sadism', 'santorum', 'scat', 'schlong', 'scissoring', 'semen', 'sex', 'sexcam', 'sexo', 'sexy', 'sexual', 'sexually', 'sexuality', 'shemale', 'shibari', 'shit', 'shitblimp', 'shitty', 'shota', 'shrimping', 'skeet', 'slanteye', 'slut', 's&m', 'smut', 'snatch', 'snowballing', 'sodomize', 'sodomy', 'spastic', 'spic', 'splooge', 'spooge', 'spunk', 'strapon', 'strappado', 'suck', 'sucks', 'swastika', 'swinger', 'threesome', 'throating', 'thumbzilla', 'tit', 'tits', 'titties', 'titty', 'topless', 'tosser', 'towelhead', 'tranny', 'tribadism', 'tubgirl', 'tushy', 'twat', 'twink', 'twinkie', 'undressing', 'upskirt', 'urophilia', 'vagina', 'viagra', 'vibrator', 'vorarephilia', 'voyeur', 'voyeurweb', 'voyuer', 'vulva', 'wank', 'wetback', 'whore', 'worldsex', 'xx', 'xxx', 'yaoi', 'yiffy', 'zoophilia']
-
-    const nice_words = ["angel", "nice person", "darling", "treasure", "endearing person", "gentleman", "salt of the earth", "genius", ]
-
-    for (let i = 0; i < wordArray.length; i++) {
-        if (bad_words.includes(wordArray[i])){
-            wordArray[i] = nice_words[Math.floor(Math.random()*nice_words.length)]
+    axios.get("http://127.0.0.1:5000/get_result/"+inputed_text).then(res => 
+        {
+            console.log(res);
+            document.getElementById("suggestion-text").textContent = res.data.serverReply;
+            console.log(res.data.serverReply)
         }
-        else{
-            console.log("something is wrong")
-        }
-        ;
-      }
-    
-      const final_text = wordArray.join(' ')
+    );
 
-      return document.getElementById("suggestion-text").textContent = final_text;
-    
-    
 };
-
-//function suggest(correction_words) {
-   
-//};
 
 export default MainSite;
--- python_backend_AI.py
+++ python_backend_AI.py
@@ -1,48 +1,47 @@
 import spacy
 nlp = spacy.load('en_core_web_sm')
-text = input("Enter the statement to be niced: ")
+from flask import Flask 
+from flask_cors import CORS, cross_origin
+import random
 
-about_text = (text)
+app = Flask(__name__)
 
-about_doc = nlp(about_text)
-        
-words = [token for token in about_doc]
+BAD_WORDS = ["fuck", "dumbass", "acrotomophilia", 'anal', 'anilingus', 'anus', 'apeshit', 'arsehole', 'ass', 'asshole', 'assmunch', 'autoerotic', 'babeland', 'bangbros', 'bangbus', 'bareback', 'barenaked', 'bastard', 'bastardo', 'bastinado', 'bbw', 'bdsm', 'beaner', 'beaners', 'beastiality', 'bestiality', 'bimbos', 'birdlock', 'bitch', 'bitches', 'blowjob', 'blumpkin', 'bollocks', 'bondage', 'boner', 'boob', 'boobs', 'bukkake', 'bulldyke', 'bullshit', 'bunghole', 'busty', 'butt', 'buttcheeks', 'butthole', 'camgirl', 'camslut', 'camwhore', 'carpetmuncher', 'cialis', 'circlejerk', 'clit', 'clitoris', 'clusterfuck', 'cock', 'cocks', 'coprolagnia', 'coprophilia', 'cornhole', 'coon', 'coons', 'creampie', 'cum', 'cumming', 'cumshot', 'cumshots', 'cunnilingus', 'cunt', 'darkie', 'daterape', 'deepthroat', 'dendrophilia', 'dick', 'dildo', 'dingleberry', 'dingleberries', 'doggiestyle', 'doggystyle', 'dolcett', 'domination', 'dominatrix', 'dommes', 'dvda', 'ecchi', 'ejaculation', 'erotic', 'erotism', 'escort', 'eunuch', 'fag', 'faggot', 'fecal', 'felch', 'fellatio', 'feltch', 'femdom', 'figging', 'fingerbang', 'fingering', 'fisting', 'footjob', 'frotting', 'fuck', 'fuckin', 'fucking', 'fucktards', 'fudgepacker', 'futanari', 'gangbang', 'genitals', 'goatcx', 'goatse', 'gokkun', 'goodpoop', 'goregasm', 'grope', 'g-spot', 'guro', 'handjob', 'hardcore', 'hentai', 'homoerotic', 'honkey', 'hooker', 'horny', 'humping', 'incest', 'intercourse', 'jailbait', 'jigaboo', 'jiggaboo', 'jiggerboo', 'jizz', 'juggs', 'kike', 'kinbaku', 'kinkster', 'kinky', 'knobbing', 'livesex', 'lolita', 'lovemaking', 'masturbate', 'masturbating', 'masturbation', 'milf', 'mong', 'motherfucker', 'muffdiving', 'nambla', 'nawashi', 'negro', 'neonazi', 'nigga', 'nigger', 'nimphomania', 'nipple', 'nipples', 'nsfw', 'nude', 'nudity', 'nutten', 'nympho', 'nymphomania', 'octopussy', 'omorashi', 'orgasm', 'orgy', 'paedophile', 'paki', 'panties', 'panty', 'pedobear', 'pedophile', 'pegging', 'penis', 'pikey', 'pissing', 'pisspig', 'playboy', 'ponyplay', 'poof', 'poon', 'poontang', 'punany', 'poopchute', 'porn', 'porno', 'pornography', 'pthc', 'pubes', 'pussy', 'queaf', 'queef', 'quim', 'raghead', 'rape', 'raping', 'rapist', 'rectum', 'rimjob', 'rimming', 'sadism', 'santorum', 'scat', 'schlong', 'scissoring', 'semen', 'sex', 'sexcam', 'sexo', 'sexy', 'sexual', 'sexually', 'sexuality', 'shemale', 'shibari', 'shit', 'shitblimp', 'shitty', 'shota', 'shrimping', 'skeet', 'slanteye', 'slut', 's&m', 'smut', 'snatch', 'snowballing', 'sodomize', 'sodomy', 'spastic', 'spic', 'splooge', 'spooge', 'spunk', 'strapon', 'strappado', 'suck', 'sucks', 'swastika', 'swinger', 'threesome', 'throating', 'thumbzilla', 'tit', 'tits', 'titties', 'titty', 'topless', 'tosser', 'towelhead', 'tranny', 'tribadism', 'tubgirl', 'tushy', 'twat', 'twink', 'twinkie', 'undressing', 'upskirt', 'urophilia', 'vagina', 'viagra', 'vibrator', 'vorarephilia', 'voyeur', 'voyeurweb', 'voyuer', 'vulva', 'wank', 'wetback', 'whore', 'worldsex', 'xx', 'xxx', 'yaoi', 'yiffy', 'zoophilia']
+NOUNS = ["noble", "good person", "gentleman", "queen", "lady"]
+ADJECTIVES = ["nice", "kind", "beautiful", "amazing", "awesome"]
+ADVERBS = ["cheerfully", "happily", "kindly", "lovingly", "sweetly"]
+VERBS = ["helped", "saved", "rescued", "protected", "saved"]
 
-"""
-#words = [token for token in about_doc if not token.is_stop and not token.is_punct]
-#code used if implemented with website
-"""
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/get_result/<user_data>", methods=["GET"])
+def get_result(user_data):
+    text = user_data
+    about_text = (text)
+    about_doc = nlp(about_text)
+    words = [token for token in about_doc]
+    #words = [token for token in about_doc if not token.is_stop and not token.is_punct]
+
+    final_sentence = ""
     
-final_sentence = ""
-for token in words:
-    if token.pos_ == 'NOUN':
-        final_sentence += 'gentleman '
-    elif token.pos_ == 'ADJ':
-        final_sentence += 'amazing '
-    elif token.pos_ == 'ADV':
-        final_sentence += 'cheerfully '
-    elif token.pos_ == 'VERB':
-        final_sentence += 'helped '
-    else:
-        final_sentence += (str(token) + ' ')
-        
-"""
-n = []
-adj = []
-adv = []
-v = []
-for token in words:
-    if token.pos_ == 'NOUN':
-        n.append(token)
-    elif token.pos_ == 'ADJ':
-        adj.append(token)
-    elif token.pos_ == 'ADV':
-        adv.append(token)
-    elif token.pos_ == 'VERB':
-        v.append(token)
+    for token in words:
+        if token.lemma_ in BAD_WORDS:
+            if token.pos_ == 'NOUN':
+                final_sentence += random.choice(NOUNS) + ' '
+            elif token.pos_ == 'ADJ':
+                final_sentence += random.choice(ADJECTIVES) + ' '
+            elif token.pos_ == 'ADV':
+                final_sentence += random.choice(ADVERBS) + ' '
+            elif token.pos_ == 'VERB':
+                final_sentence += random.choice(VERBS) + ' '
+        else:
+            final_sentence += (str(token) + ' ')
+    
+    print(final_sentence)
 
-This code is used if implemented in the website. We use lists to provide a wider range
-of nice word choices.
-"""
-
-print("Here is a nicer way to say your statement: " + final_sentence)
+    return {"serverReply": str(final_sentence)}
+    
+if __name__ == "__main__":
+    app.run(debug=True)
